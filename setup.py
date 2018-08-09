09df8bc79a351059e01fc78eb7bbfac762afed76
Diego Rodriguez <diego.rodriguez@cern.ch>
2018-08-09 16:24:31 +0200

    travis: add matrix with Python 2.7 and 3.6-7

--- setup.py
+++ setup.py
@@ -108,6 +108,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
