69834ee45769722cc87a5863d9ab56776b66c34f
Tibor Šimko <tibor.simko@cern.ch>
2019-12-27 14:11:10 +0100

    version: post-release bump
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- reana/version.py
+++ reana/version.py
@@ -13,4 +13,4 @@ This file is imported by ``reana.__init__`` and parsed by ``setup.py``.
 
 from __future__ import absolute_import, print_function
 
-__version__ = "0.6.0"
+__version__ = "0.7.0.dev20191227"
