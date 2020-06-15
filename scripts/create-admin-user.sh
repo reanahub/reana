240a4c5b8def019c7e187952046f8d20f1c5c3b4
Tibor Šimko <tibor.simko@cern.ch>
2020-06-15 21:16:55 +0200

    scripts: configurable admin email address
    Amends `create-admin-user.sh` to make admin email address configurable.
    
    Signed-off-by: Tibor Šimko <tibor.simko@cern.ch>

--- scripts/create-admin-user.sh
+++ scripts/create-admin-user.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Two parameters: instance name and email address of the admin
 instance_name=$1
+if [ -z "$instance_name" ]; then
+    instance_name=reana
+fi
+email_address=$2
+if [ -z "$email_address" ]; then
+    email_address=root@localhost
+fi
 
 # Get REANA Server pod name
 REANA_SERVER=$(kubectl get pod -l "app=$instance_name-server" | grep Running | awk '{print $1}')
@@ -10,7 +18,7 @@ kubectl exec "$REANA_SERVER" -- ./scripts/setup
 
 # Create admin user
 admin_access_token=$(kubectl exec "$REANA_SERVER" -- \
-                     flask reana-admin create-admin-user admin@reana.org)
+                     flask reana-admin create-admin-user $email_address)
 
 # Add token to secrets
 kubectl create secret generic "$instance_name"-admin-access-token \
