35a12ac7ec9e27d9cc7ec2f10f7251171b21c4da
Tibor Šimko <tibor.simko@cern.ch>
2021-06-24 14:53:48 +0200

    release: 0.8.0-alpha.2

--- scripts/prefetch-images.sh
+++ scripts/prefetch-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This file is part of REANA.
-# Copyright (C) 2020 CERN.
+# Copyright (C) 2020, 2021 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -11,14 +11,14 @@ for image in \
     postgres:9.6.2 \
     redis:5.0.5 \
     jupyter/scipy-notebook \
-    reanahub/reana-job-controller:0.8.0-alpha.2 \
-    reanahub/reana-message-broker:0.7.0 \
-    reanahub/reana-server:0.8.0-alpha.1 \
-    reanahub/reana-ui:0.8.0-alpha.1 \
-    reanahub/reana-workflow-controller:0.8.0-alpha.1 \
-    reanahub/reana-workflow-engine-cwl:0.8.0-alpha.1 \
-    reanahub/reana-workflow-engine-serial:0.8.0-alpha.1 \
-    reanahub/reana-workflow-engine-yadage:0.8.0-alpha.1; do
+    reanahub/reana-job-controller:0.8.0-alpha.3 \
+    reanahub/reana-message-broker:0.8.0-alpha.1 \
+    reanahub/reana-server:0.8.0-alpha.2 \
+    reanahub/reana-ui:0.8.0-alpha.2 \
+    reanahub/reana-workflow-controller:0.8.0-alpha.2 \
+    reanahub/reana-workflow-engine-cwl:0.8.0-alpha.2 \
+    reanahub/reana-workflow-engine-serial:0.8.0-alpha.2 \
+    reanahub/reana-workflow-engine-yadage:0.8.0-alpha.2; do
     docker pull $image
     kind load docker-image $image
 done
