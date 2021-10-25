83fc369ba635b8df1c0d494b440ed2c3ff88e632
REANA team <team@reana.io>
2021-10-25 21:13:51 +0200

    global: fix copyright years

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of REANA.
-# Copyright (C) 2017, 2018 CERN.
+# Copyright (C) 2017, 2018, 2020, 2021 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
