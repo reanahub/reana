1cc3f6796c7de8a4577847a377c671ea2bf6add9
Tibor Šimko <tibor.simko@cern.ch>
2022-06-20 12:23:10 +0200

    config: new reana-client-go repository

--- reana/config.py
+++ reana/config.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of REANA.
-# Copyright (C) 2020, 2021 CERN.
+# Copyright (C) 2020, 2021, 2022 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -39,6 +39,7 @@ REPO_LIST_ALL = [
     "reana-auth-krb5",
     "reana-auth-vomsproxy",
     "reana-client",
+    "reana-client-go",
     "reana-commons",
     "reana-db",
     "reana-demo-cdci-crab-pulsar-integral-verification",
