aab27461196e9adacf310914ff35f1c34c56ffb4
alextintin007 <alextintin007@outlook.com>
2024-06-08 16:29:08 -0500

    REANA and Kueue Test

--- scripts/prefetch-images.sh
+++ scripts/prefetch-images.sh
@@ -11,11 +11,11 @@ for image in \
     docker.io/maildev/maildev:1.1.0 \
     docker.io/library/postgres:12.13 \
     docker.io/library/redis:5.0.5 \
-    docker.io/reanahub/reana-job-controller:0.9.2 \
+    docker.io/alextintin007/reana-job-controller:v1.0 \
     docker.io/reanahub/reana-message-broker:0.9.2 \
     docker.io/reanahub/reana-server:0.9.2 \
     docker.io/reanahub/reana-ui:0.9.3 \
-    docker.io/reanahub/reana-workflow-controller:0.9.2 \
+    docker.io/alextintin007/reana-workflow-controller:v1.0 \
     docker.io/reanahub/reana-workflow-engine-cwl:0.9.2 \
     docker.io/reanahub/reana-workflow-engine-serial:0.9.2 \
     docker.io/reanahub/reana-workflow-engine-snakemake:0.9.2 \
