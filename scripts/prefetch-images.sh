1ca9deaf1b73e18774019cf1e0cb5cc1fb1c3016
Tibor Šimko <tibor.simko@cern.ch>
2024-11-22 11:44:35 +0100

    feat(scripts): upgrade to Jupyter SciPy 7.2.2 notebook (#846)

--- scripts/prefetch-images.sh
+++ scripts/prefetch-images.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
 # This file is part of REANA.
-# Copyright (C) 2020, 2021, 2022, 2023 CERN.
+# Copyright (C) 2020, 2021, 2022, 2023, 2024 CERN.
 #
 # REANA is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
 for image in \
-    docker.io/jupyter/scipy-notebook:notebook-6.4.5 \
     docker.io/maildev/maildev:1.1.0 \
     docker.io/library/postgres:12.13 \
     docker.io/library/redis:5.0.5 \
@@ -20,6 +19,7 @@ for image in \
     docker.io/reanahub/reana-workflow-engine-serial:0.9.3 \
     docker.io/reanahub/reana-workflow-engine-snakemake:0.9.3 \
     docker.io/reanahub/reana-workflow-engine-yadage:0.9.4 \
+    quay.io/jupyter/scipy-notebook:notebook-7.2.2 \
     ; do
         docker pull $image
         kind load docker-image $image
