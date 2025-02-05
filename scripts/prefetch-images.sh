251a172977e0eb666d4335809ef43ca692fcfeab
Tibor Šimko <tibor.simko@cern.ch>
2025-02-05 15:02:33 +0100

    chore(master): release 0.95.0-alpha.1
    Release-As: 0.95.0

--- scripts/prefetch-images.sh
+++ scripts/prefetch-images.sh
@@ -10,15 +10,15 @@ for image in \
     docker.io/maildev/maildev:1.1.0 \
     docker.io/library/postgres:12.13 \
     docker.io/library/redis:5.0.5 \
-    docker.io/reanahub/reana-job-controller:0.9.4 \
-    docker.io/reanahub/reana-message-broker:0.9.3 \
-    docker.io/reanahub/reana-server:0.9.4 \
-    docker.io/reanahub/reana-ui:0.9.4 \
-    docker.io/reanahub/reana-workflow-controller:0.9.4 \
-    docker.io/reanahub/reana-workflow-engine-cwl:0.9.4 \
-    docker.io/reanahub/reana-workflow-engine-serial:0.9.4 \
-    docker.io/reanahub/reana-workflow-engine-snakemake:0.9.4 \
-    docker.io/reanahub/reana-workflow-engine-yadage:0.9.5 \
+    docker.io/reanahub/reana-job-controller:0.95.0-alpha.2 \
+    docker.io/reanahub/reana-message-broker:0.95.0-alpha.1 \
+    docker.io/reanahub/reana-server:0.95.0-alpha.2 \
+    docker.io/reanahub/reana-ui:0.95.0-alpha.1 \
+    docker.io/reanahub/reana-workflow-controller:0.95.0-alpha.2 \
+    docker.io/reanahub/reana-workflow-engine-cwl:0.95.0-alpha.2 \
+    docker.io/reanahub/reana-workflow-engine-serial:0.95.0-alpha.2 \
+    docker.io/reanahub/reana-workflow-engine-snakemake:0.95.0-alpha.2 \
+    docker.io/reanahub/reana-workflow-engine-yadage:0.95.0-alpha.2 \
     quay.io/jupyter/scipy-notebook:notebook-7.2.2; do
     docker pull $image
     kind load docker-image $image
